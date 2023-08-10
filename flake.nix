7977c7bbd0d5997b33e5de3e32a66ae7c51370a4
Jacek Galowicz <jacek@galowicz.de>
2023-08-10 10:09:12 +0200

    Add crane and rust checks

--- flake.nix
+++ flake.nix
@@ -5,6 +5,10 @@
     flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
+    advisory-db.url = "github:rustsec/advisory-db";
+    advisory-db.flake = false;
+    crane.url = "github:ipetkov/crane";
+    crane.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } {
@@ -14,51 +18,62 @@
       "x86_64-darwin"
       "aarch64-darwin"
     ];
-    perSystem = { config, pkgs, system, ... }: {
-      devShells.default = pkgs.mkShell {
-        inputsFrom = builtins.attrValues config.checks;
-        inherit (config.checks.pre-commit-check) shellHook;
-      };
-
-      packages = {
-        hello-cpp = pkgs.stdenv.mkDerivation {
-          name = "hello-cpp";
-          src = ./cpp;
-          nativeBuildInputs = [ pkgs.cmake ];
-          buildInputs = [ pkgs.boost ];
+    perSystem = { config, pkgs, system, ... }:
+      let
+        craneLib = inputs.crane.lib.${system};
+        src = craneLib.cleanCargoSource (craneLib.path ./rust);
+        cargoArtifacts = craneLib.buildDepsOnly { inherit src; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          inputsFrom = builtins.attrValues config.checks;
+          inherit (config.checks.pre-commit-check) shellHook;
         };
-        hello-rust = pkgs.rustPlatform.buildRustPackage {
-          name = "hello-rust";
-          src = ./rust;
-          cargoLock.lockFile = ./rust/Cargo.lock;
-        };
-      };
 
-      checks = {
-        inherit (config.packages)
-          hello-cpp
-          hello-rust
-          ;
-
-        pre-commit-check = inputs.pre-commit-hooks.lib.${system}.run {
-          src = ./.;
-          hooks = {
-            # Rust
-            clippy.enable = true;
-            rustfmt.enable = true;
-
-            # Nix
-            deadnix.enable = true;
-            nixpkgs-fmt.enable = true;
-            statix.enable = true;
-
-            # Shell
-            shellcheck.enable = true;
-            shfmt.enable = true;
+        packages = {
+          hello-cpp = pkgs.stdenv.mkDerivation {
+            name = "hello-cpp";
+            src = ./cpp;
+            nativeBuildInputs = [ pkgs.cmake ];
+            buildInputs = [ pkgs.boost ];
           };
-          settings.rust.cargoManifestPath = "./rust/Cargo.toml";
+          hello-rust = craneLib.buildPackage { inherit cargoArtifacts src; };
+        };
+
+        checks = {
+          inherit (config.packages)
+            hello-cpp
+            hello-rust
+            ;
+
+          hello-rust-doc = craneLib.cargoDoc {
+            inherit cargoArtifacts src;
+          };
+
+          hello-rust-audit = craneLib.cargoAudit {
+            inherit (inputs) advisory-db;
+            inherit src;
+          };
+
+          pre-commit-check = inputs.pre-commit-hooks.lib.${system}.run {
+            src = ./.;
+            hooks = {
+              # Rust
+              clippy.enable = true;
+              rustfmt.enable = true;
+
+              # Nix
+              deadnix.enable = true;
+              nixpkgs-fmt.enable = true;
+              statix.enable = true;
+
+              # Shell
+              shellcheck.enable = true;
+              shfmt.enable = true;
+            };
+            settings.rust.cargoManifestPath = "./rust/Cargo.toml";
+          };
         };
       };
-    };
   };
 }
